2dbcfec752fd6a95dcf34da74ffe20b971e3ef75
Jun <lhcjun@gmail.com>
2020-03-28 14:32:35 +0800

    connect server

--- src/Assets/apiCall.js
+++ src/Assets/apiCall.js
@@ -1,8 +1,8 @@
 export const API_CALL = {
-    URL: 'http://localhost:3001/',
-    SIGNIN: 'http://localhost:3001/signIn',
-    REGISTER: 'http://localhost:3001/register',
-    PROFILE_ID: 'http://localhost:3001/profile/:id',
-    IMAGE: 'http://localhost:3001/image',
-    IMAGE_URL: 'http://localhost:3001/imageUrl'
+    URL: 'https://arcane-mountain-61525.herokuapp.com/',
+    SIGNIN: 'https://arcane-mountain-61525.herokuapp.com/signIn',
+    REGISTER: 'https://arcane-mountain-61525.herokuapp.com/register',
+    PROFILE_ID: 'https://arcane-mountain-61525.herokuapp.com/profile/:id',
+    IMAGE: 'https://arcane-mountain-61525.herokuapp.com/image',
+    IMAGE_URL: 'https://arcane-mountain-61525.herokuapp.com/imageUrl'
 }

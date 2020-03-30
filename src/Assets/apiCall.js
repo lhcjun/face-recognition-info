b8eefe8fc9241b24957f60a60ecb49164225d4f3
Jun <lhcjun@gmail.com>
2020-03-30 13:44:29 +0800

    cors

--- src/Assets/apiCall.js
+++ src/Assets/apiCall.js
@@ -1,8 +1,10 @@
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
 export const API_CALL = {
     URL: 'https://arcane-mountain-61525.herokuapp.com/',
     SIGNIN: 'https://arcane-mountain-61525.herokuapp.com/signIn',
     REGISTER: 'https://arcane-mountain-61525.herokuapp.com/register',
     PROFILE_ID: 'https://arcane-mountain-61525.herokuapp.com/profile/:id',
     IMAGE: 'https://arcane-mountain-61525.herokuapp.com/image',
-    IMAGE_URL: 'https://arcane-mountain-61525.herokuapp.com/imageUrl'
+    IMAGE_URL: proxyUrl + 'https://arcane-mountain-61525.herokuapp.com/imageUrl'
 }

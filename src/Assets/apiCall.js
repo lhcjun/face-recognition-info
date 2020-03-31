962d6b9a4f1a2f65184b5baf63d148e27ceeedd8
Jun <lhcjun@gmail.com>
2020-03-31 21:58:29 +0800

    fix CORS & 413 error

--- src/Assets/apiCall.js
+++ src/Assets/apiCall.js
@@ -1,4 +1,4 @@
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+// const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 export const API_CALL = {
     URL: 'https://arcane-mountain-61525.herokuapp.com/',
@@ -6,5 +6,5 @@ export const API_CALL = {
     REGISTER: 'https://arcane-mountain-61525.herokuapp.com/register',
     PROFILE_ID: 'https://arcane-mountain-61525.herokuapp.com/profile/:id',
     IMAGE: 'https://arcane-mountain-61525.herokuapp.com/image',
-    IMAGE_URL: proxyUrl + 'https://arcane-mountain-61525.herokuapp.com/imageUrl'
+    IMAGE_URL:  'https://arcane-mountain-61525.herokuapp.com/imageUrl'
 }
